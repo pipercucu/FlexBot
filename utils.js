3dbe9a85eb9c1d87aa26e3a0cc5adddee736029b
Piper <pipercucurbit@gmail.com>
2020-05-05 12:52:00 -0400

    Util method documentation.

--- utils.js
+++ utils.js
@@ -6,6 +6,16 @@ module.exports = {
   toTitleCase: toTitleCase
 }
 
+/**
+ * Pads a string with characters. Used mainly for keeping table columns aligned for price and position displays
+ * e.g we want the price column to always be a consistent width 
+ *    ticker | price        | 24hr % chg
+ * +     XMR | $60.17       | 0.1
+ * +     BTC | $8868.77     | 0.55
+ * @param {string} pad String we're padding with, usually a number of spaces, e.g. '       '
+ * @param {string} str String that we're padding e.g. '60.17'
+ * @param {boolean} padLeft If true, then padding is on the left of the string, otherwise it's on the right
+*/
 function padString(pad, str, padLeft) {
   if (typeof str === 'undefined') 
     return pad;
@@ -16,6 +26,10 @@ function padString(pad, str, padLeft) {
   }
 }
 
+/**
+ * See if a string matches a discord user identifier
+ * @param {string} str String to check
+*/
 function parseDiscordUserId(str) {
   let pattern = /<@![0-9]+>/g;
   let matches = str.match(pattern);
@@ -27,6 +41,10 @@ function parseDiscordUserId(str) {
   }
 }
 
+/**
+ * Change a string to proper case, e.g. 'heya there' becomes 'Heya There'
+ * @param {string} str String to check
+*/
 function toTitleCase(str) {
   return str.replace(
     /\w\S*/g,
